9adbf77c18a8d95f0a2bf07129b0a4418a8238ba
getsolutions4you <getsolutions4you@gmail.com>
2023-04-10 22:13:19 +0530

    Name Changes

--- CharToASCII.java
+++ CharToASCII.java
@@ -2,7 +2,7 @@ public class CharToASCII {
     public static void main(String[] args) {
         char var = 'C';
         int var2 = var;
-        System.out.println("ASCII value is "+var2);
+        System.out.println("ASCII value: "+var2);
 
     }
 }
